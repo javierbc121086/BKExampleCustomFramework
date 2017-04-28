d2160de6b8e6e1104e7db8f20719fed8c670108b
Javier <javier.bc121086@gmail.com>
2017-04-28 15:28:23 -0500

    initial commit

---
+++ ExampleCustomFramework/ExampleCustomFramework.h
@@ -0,0 +1,19 @@
+//
+//  ExampleCustomFramework.h
+//  ExampleCustomFramework
+//
+//  Created by Javier Bolaños on 28/04/17.
+//  Copyright © 2017 beekeeperk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleCustomFramework.
+FOUNDATION_EXPORT double ExampleCustomFrameworkVersionNumber;
+
+//! Project version string for ExampleCustomFramework.
+FOUNDATION_EXPORT const unsigned char ExampleCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleCustomFramework/PublicHeader.h>
+
+
